bc7c1ba70b42a28faa16718bc11d1adfc5b021f7
Alessandro DiMarco <8059120+alesso-x@users.noreply.github.com>
2021-02-22 20:18:25 -0500

    fix less order with postcss

--- packages/react-scripts/scripts/start.js
+++ packages/react-scripts/scripts/start.js
@@ -156,8 +156,7 @@ checkBrowsers(paths.appPath, isInteractive)
       }
 
       console.log(chalk.cyan('Starting the development server...\n'));
-      // @joor disabled open browser
-      // openBrowser(urls.localUrlForBrowser);
+      openBrowser(urls.localUrlForBrowser);
     });
 
     ['SIGINT', 'SIGTERM'].forEach(function (sig) {
